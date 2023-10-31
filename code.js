8e7a008a8663ba1a8a70cd53a5cae67f091498a4
Sean W <104464288+swilso59@users.noreply.github.com>
2023-10-31 12:12:27 -0600

    Update code.js
    Updated code

--- code.js
+++ code.js
@@ -1,21 +1,19 @@
 function fib(n) {
-    if n (<= 0){
+    if (n <= 0){
     return [];
     }
     else if (n === 1){
         return [0];
     }
-    else if (n ===2){
-        return [0,1]; 
+    else if (n === 2){
+        return [0,1,1]; 
     }
     else {
-        const fibArr = fib(n-1);
-        const nextFib = fib(n-1) + fib(n-2);
-        fibArr.push(nextFib);
+        var fibArr = fib(n-1);
+        
+        fibArr.push(fibArr[n-2] + fibArr[n-1]);
         return fibArr;
     }
 }
 
-const n = 10;
-const result = fib(n);
-console.log(result);
+console.log(fib(20));
