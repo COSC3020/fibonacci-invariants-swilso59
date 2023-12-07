83203c6b1301be2e6817b52a7b1b987f587d35ed
Sean W <104464288+swilso59@users.noreply.github.com>
2023-12-06 18:51:27 -0700

    Update code.js
    Corrections made

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function fib(n) {
-    if (n <= 0){
-    return [];
+    if (n === 0){
+    return [0];
     }
     else if (n === 1){
-        return [0];
+        return [0,1];
     }
     else if (n === 2){
         return [0,1,1]; 
@@ -17,3 +17,5 @@ function fib(n) {
 }
 
 console.log(fib(20));
+
+// Made corrections to the if statment for n === 0, and else if statment for n === 1. Seems to work. 
